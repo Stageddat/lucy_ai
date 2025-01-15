4029d01f7841e8f7810781dd521a7e47c34ef2fb
Stageddat <chmarc72@gmail.com>
2025-01-15 15:36:16 +0100

    fix: remove useless api keys

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-import os
-
 import discord
-import dotenv
 from discord.ext import commands
 
 bot = discord.Bot()
@@ -10,10 +7,3 @@ intents.message_content = True
 intents.messages = True
 intents.guild_messages = True
 bot = commands.Bot(command_prefix="a!", intents=intents)
-
-# Server ID
-guild_id = 815909997186777128
-
-# Github token
-dotenv.load_dotenv()
-github_token = str(os.getenv("GITHUB_TOKEN"))
